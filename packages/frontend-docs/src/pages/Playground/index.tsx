3444fb8d4e58f9d02af51f4cd15e9741eb5f1033
RaihanArdianata <raihan.ardianata@gmail.com>
2026-03-01 22:31:16 +0700

    update page

--- packages/frontend-docs/src/pages/Playground/index.tsx
+++ packages/frontend-docs/src/pages/Playground/index.tsx
@@ -1,6 +1,21 @@
 import PayIDPlayground from '@site/src/components/Playground';
 import { ReactNode } from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
 
 export default function Playground(): ReactNode {
-  return <PayIDPlayground />;
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      {/* <HomepageHeader /> */}
+      {/* <Header /> */}
+
+      <main>
+        <PayIDPlayground />;{' '}
+      </main>
+    </Layout>
+  );
 }
